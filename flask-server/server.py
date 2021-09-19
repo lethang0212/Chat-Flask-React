54347c73bb84ec876b37644ea58f133b79705c05
lethang0212 <thangld@vnext.com.vn>
2021-09-19 20:01:37 +0700

    project

---
+++ flask-server/server.py
@@ -0,0 +1,15 @@
+from logging import debug
+from flask import Flask
+
+app = Flask(__name__)
+
+# Api route memberteam
+
+
+@app.route("/members")
+def members():
+    return {"members": ["Thang", "Quy", "Than", "Thieu", "Khang"]}
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
