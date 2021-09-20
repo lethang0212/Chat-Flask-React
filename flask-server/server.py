00fc582b90589a8884863374ac9ba96f354ef1e3
SmilerKai <ihado.lethang@gmail.com>
2021-09-20 13:15:05 +0700

    structbase react and api basic

--- flask-server/server.py
+++ flask-server/server.py
@@ -6,9 +6,18 @@ app = Flask(__name__)
 # Api route memberteam
 # test a
 
+
 @app.route("/members")
 def members():
-    return {"members": ["Thang", "Quy", "Than", "Thieu", "Khang"]}
+    return {
+        "members": [
+            {"id": 1, "name": "le thang", "address": "Phuong Trung, Ha Noi"},
+            {"id": 2, "name": "le quy", "address": "Phuong Trung, Ha Noi"},
+            {"id": 3, "name": "le than", "address": "Phuong Trung, Ha Noi"},
+            {"id": 4, "name": "le thieu", "address": "Phuong Trung, Ha Noi"},
+            {"id": 5, "name": "le khang", "address": "Phuong Trung, Ha Noi"},
+        ],
+    }
 
 
 if __name__ == "__main__":
