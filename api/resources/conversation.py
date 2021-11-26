b9846fd5557ec09919ca9fd324f2c8bd40a8c7d8
thandepzai <91040602+thandepzai@users.noreply.github.com>
2021-11-26 09:07:30 +0700

    Update comment

--- api/resources/conversation.py
+++ api/resources/conversation.py
@@ -12,9 +12,9 @@ from api.resources.db import get_db
 bp = Blueprint('conversation', __name__, url_prefix='/api') 
 api = Api(bp)
 
-class conversation(Resource):
+class conversation(Resource): #Lấy tất cả các message từ 1 conversation của 1 user
     @jwt_required()
-    def get(self,guid):
+    def get(self,guid): #Nhận token của user, id của cuộc trò chuyện(guid) nếu user thuộc cuộc trò chuyện thì return
         db = get_db()
         query = f"SELECT * FROM message WHERE guid = {guid}"
         message = db.execute(query).fetchone()
@@ -30,9 +30,9 @@ class conversation(Resource):
         messages = [dict((cursor.description[i][0], val) for i, val in enumerate(row)) for row in cursor.execute(query)]
         return messages,200
 
-class chatList(Resource):
+class chatList(Resource): # lấy tất cả các conversation của một user liệt kê theo thứ tự thời gian update gần nhất
     @jwt_required()
-    def get(self,uid):
+    def get(self,uid): #nhận token của user, id của user muốn lấy conversation để so sánh nếu đúng thì return 
         uid1 = get_jwt_identity()
         if uid1 != int(uid): return{"msg":"Not have access"},203
         db = get_db()
