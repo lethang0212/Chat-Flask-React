99e00f56cf65f4718e387619b0b575b4d5fd66aa
Quy Nguyen <youngboy5801@gmail.com>
2021-12-02 23:54:30 +0700

    refractor socket to resources.events

--- app.py
+++ app.py
@@ -3,10 +3,5 @@ from flask_socketio import emit
 
 app = create_app()
 
-@socketio.on('my response')
-def test_response(message, methods=['GET', 'POST']):
-    emit('my response', message, broadcast=True)
-    print(message)
-
 if __name__ == '__main__':
     socketio.run(app, debug=True)
