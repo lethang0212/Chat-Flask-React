b0c0bc9d84beebd7a7d3528c4dc20d6d89ea580c
Quy Nguyen <youngboy5801@gmail.com>
2021-12-02 22:43:20 +0700

    fix db

---
+++ app.py
@@ -0,0 +1,13 @@
+from api import create_app, socketio
+from flask_socketio import emit
+
+app = create_app()
+
+@socketio.on('my response')
+
+def test_response(message, methods=['GET', 'POST']):
+    # emit('my response', {'data': message}, broadcast=True)
+    print('received stuffs: ', message)
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
