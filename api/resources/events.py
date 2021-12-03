95c09421af6bcfff13087e0f4b806140531cec19
Quy Nguyen <youngboy5801@gmail.com>
2021-12-03 16:56:10 +0700

    add event join and handle msg

--- api/resources/events.py
+++ api/resources/events.py
@@ -1,7 +1,12 @@
 from api import socketio
-from flask_socketio import emit
+from flask_socketio import emit, join_room
 
-@socketio.on('my response')
-def test_response(message, methods=['GET', 'POST']):
-    emit('my response', {'data': message}, broadcast=True)
-    print('received stuffs: ', message)
+@socketio.on('logged_in')
+def join_all_convs(convs):
+    for conv in convs:
+        join_room(conv['guid'])
+
+
+@socketio.on('server_listening')
+def handle_message(msg):
+    emit('client_listening', msg, to=msg['guid'], broadcast=True)
